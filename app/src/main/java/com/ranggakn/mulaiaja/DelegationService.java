403322ac8e84b2394fcedd7e0726b956f8fc0e35
ranggakn <adi710797@gmail.com>
2026-06-08 01:00:42 +0700

    Ubah home url dan icon aplikasi

---
+++ app/src/main/java/com/ranggakn/mulaiaja/DelegationService.java
@@ -0,0 +1,14 @@
+package com.ranggakn.mulaiaja;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
